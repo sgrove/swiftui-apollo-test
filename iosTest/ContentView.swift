5be014c31aff5a36f13da08248cf25b379a84458
Sean Grove <sean@bushi.do>
2019-12-29 23:02:44 -0800

    Initial Commit

---
+++ iosTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosTest
+//
+//  Created by Sean Grove on 12/29/19.
+//  Copyright © 2019 Sean Grove. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
